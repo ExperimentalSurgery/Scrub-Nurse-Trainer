b9cd85173fbe9fd3e08d36227438262469d8dccc
peschler-nmy <p.eschler@nmy.de>
2024-03-28 11:12:47 +0100

    Removed unused outline plugin namespace

--- GreifbAR_OTAToolpicker/Assets/_OTAToolpicker/Scripts/PlaceableInstrument.cs
+++ GreifbAR_OTAToolpicker/Assets/_OTAToolpicker/Scripts/PlaceableInstrument.cs
@@ -2,7 +2,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Assertions;
 using UnityEngine.Serialization;
-using EPOOutline;
 using UnityEngine.Rendering;
 
 namespace NMY.OTAToolpicker
